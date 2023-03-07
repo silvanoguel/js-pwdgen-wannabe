7a7fccdf24663ccae01b797a4a22d978d64d8429
silvanoguel <silvanoguel@msn.com>
2023-03-07 13:47:27 +0100

    add:project completed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 // RACCOLTA DATI
 // chiedere il nome all'utente
-
-
-// convertire la stringa dell'età al numero
-
+const username = prompt("Ciao utente! Come ti chiami?");
+console.log(username);
 
 // chiedere il cognome all'utente
-
+const userSurname = prompt("Qual'è il tuo cognome?");
+console.log(userSurname);
 
 // chiedere il suo colore preferito
+const userColor = prompt("Qual'è il tuo colore preferito?");
+console.log(userColor);
 
 // ELABORAZIONE DATI
-// password con aggiunta
+// creazione password con aggiunta
+const userPassword = username + userSurname + userColor + 21;
+console.log(userPassword);
 
+// Preparazione messaggio con il risultato
+const message = userPassword
 
 // OUTPUT MESSAGGIO
 // stampo il messaggio
+document.getElementById('user-password').innerHTML = message;
