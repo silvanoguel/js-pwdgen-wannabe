3af474809837dc9bd28a3d7eb41b1c9aa47ab6cc
silvanoguel <silvanoguel@msn.com>
2023-03-07 13:30:37 +0100

    add:project scaffolding

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// RACCOLTA DATI
+// chiedere il nome all'utente
+
+
+// convertire la stringa dell'età al numero
+
+
+// chiedere il cognome all'utente
+
+
+// chiedere il suo colore preferito
+
+// ELABORAZIONE DATI
+// password con aggiunta
+
+
+// OUTPUT MESSAGGIO
+// stampo il messaggio
